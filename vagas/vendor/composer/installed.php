8ffef669346434ddecaec9b1b887ee1f273782ce
Joao Gabriel Franco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2023-04-13 08:17:01 -0300

    inicializando projeto e instalando dependencias com composer

---
+++ vagas/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'crud/vagas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f98e07d08622e6cd3727a2ead0592e2a66149bcb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'crud/vagas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f98e07d08622e6cd3727a2ead0592e2a66149bcb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
